550bad410e9b7125f5e72ae60493b51ad64c0a14
teacherbot <norrisa@faytechcc.edu>
2026-09-16 14:45:24 +0000

    m2lab1 done

--- m2/m2lab1_norris.cpp
+++ m2/m2lab1_norris.cpp
@@ -17,8 +17,9 @@ int main() {
     const double CHARGE_PER_CUBIC_FOOT = 0.50;  // (Billed invoice amount per cu ft, customer cost)
     // Variables describing the crate
     double length, width, height;               // you can declare multiple of same type at once
-    double crate_cost;                          // price to make, USD
-    double crate_charge;                        // price we sell it for, USD
+    double volume;                              // V = l * w * h, in cubic ft
+    double crate_cost;                          // price to make the crate, USD per cubic ft
+    double crate_charge;                        // price we sell it for, USD per cubic ft
     double profit;                              // charge - cost
 
     // Get the dimensions of the crate
@@ -30,11 +31,24 @@ int main() {
     cout << "Crate height: ";
     cin  >> height;
 
+    // Calculate the volume (everything else depends on this value)
+    volume = length * width * height;           // cubic feet
+
     // Calculate price and cost
+    crate_cost = COST_PER_CUBIC_FOOT * volume;
+    crate_charge = CHARGE_PER_CUBIC_FOOT * volume;
 
     // Calculate profit (price - cost)
+    profit = crate_charge - crate_cost; // What they pay us, minus what we spent.
 
     // Display results to user
+    cout << setprecision(2) << fixed;   // 2 decimals for all values
+    cout << "A crate measuring " << length << " x " << width << " x " << height << " ft." << endl;
+    cout << "Is volume: " << volume << " cubic ft." << endl;
+    cout << endl;
+    cout << "Cost to build: $" << crate_cost << endl;
+    cout << "Sells for:     $" << crate_charge << endl;
+    cout << "Profit:        $" << profit << endl; 
 
     return 0; // no errors
 }
