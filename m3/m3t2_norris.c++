9956bab0a59d9fff3a1ea0fe8b9740c4d7972acd
teacherbot <norrisa@faytechcc.edu>
2026-09-23 14:29:42 +0000

    m3t2 started

---
+++ m3/m3t2_norris.c++
@@ -0,0 +1,13 @@
+// CSC 134
+// M3T2 - Random Numbers
+// norrisa
+// 9/23/26
+// Start writing a simple "Craps" casino game.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ m3/m3t2_norris.s
@@ -0,0 +1,55 @@
+	.file	"m3t2_norris.c++"
+	.intel_syntax noprefix
+	.text
+#APP
+	.globl _ZSt21ios_base_library_initv
+#NO_APP
+	.globl	main
+	.type	main, @function
+main:
+.LFB1988:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1988:
+	.size	main, .-main
+	.section	.rodata
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04.1) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
